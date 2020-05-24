273f5441f141bb4a8cd16ef1a92bb75754b6d65b
qidashe <64844066+qidashe@users.noreply.github.com>
2020-05-24 10:34:52 +0800

    后台管理

---
+++ server/后台管理系统/src/router/index.js
@@ -0,0 +1,162 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            redirect: '/dashboard'
+        },
+        {
+            path: '/',
+            component: () => import(/* webpackChunkName: "home" */ '../components/common/Home.vue'),
+            meta: { title: '自述文件' },
+            children: [
+                {
+                    path: '/dashboard',
+                    component: () => import(/* webpackChunkName: "dashboard" */ '../components/page/Dashboard.vue'),
+                    meta: { title: '系统首页' }
+                },
+                {
+                    path: '/icon',
+                    component: () => import(/* webpackChunkName: "icon" */ '../components/page/Icon.vue'),
+                    meta: { title: '自定义图标' }
+                },
+                {
+                    path: '/UserControl',
+                    component: () => import(/* webpackChunkName: "table" */ '../components/page/BaseTable.vue'),
+                    meta: { title: '用户管理' }
+                },
+                // {
+                //     path: '/tabs',
+                //     component: () => import(/* webpackChunkName: "tabs" */ '../components/page/Tabs.vue'),
+                //     meta: { title: 'tab选项卡' }
+                // },
+                {
+                    path: '/HomePost',
+                    component: () => import(/* webpackChunkName: "tabs" */ '../components/page/PostManagement/HomePost.vue'),
+                    meta: { title: '首页帖子' }
+                },
+                {
+                    path: '/lost',
+                    component: () => import(/* webpackChunkName: "tabs" */ '../components/page/PostManagement/lost.vue'),
+                    meta: { title: '失物招领' }
+                },
+                {
+                    path: '/campus',
+                    component: () => import(/* webpackChunkName: "tabs" */ '../components/page/PostManagement/campus.vue'),
+                    meta: { title: '校园风采' }
+                },
+                {
+                    path: '/CampusLife',
+                    component: () => import(/* webpackChunkName: "tabs" */ '../components/page/PostManagement/CampusLife.vue'),
+                    meta: { title: '校园生活' }
+                },
+                {
+                    path: '/CampusFun',
+                    component: () => import(/* webpackChunkName: "tabs" */ '../components/page/PostManagement/CampusFun.vue'),
+                    meta: { title: '校园帮帮' }
+                },
+                {
+                    path: '/CampusNew',
+                    component: () => import(/* webpackChunkName: "tabs" */ '../components/page/PostManagement/CampusNew.vue'),
+                    meta: { title: '校园新闻' }
+                },
+                {
+                    path: '/campusSecond',
+                    component: () => import(/* webpackChunkName: "tabs" */ '../components/page/PostManagement/campusSecond.vue'),
+                    meta: { title: '校园二手' }
+                },
+                {
+                    path: '/love',
+                    component: () => import(/* webpackChunkName: "tabs" */ '../components/page/PostManagement/love.vue'),
+                    meta: { title: '表白墙' }
+                },
+                {
+                    path: '/advertising',
+                    component: () => import(/* webpackChunkName: "tabs" */ '../components/page/PostManagement/advertising.vue'),
+                    meta: { title: '公告管理' }
+                }, {
+                    path: '/comment',
+                    component: () => import(/* webpackChunkName: "tabs" */ '../components/page/PostManagement/comment.vue'),
+                    meta: { title: '评论管理' }
+                },
+                // {
+                //     path: '/form',
+                //     component: () => import(/* webpackChunkName: "form" */ '../components/page/BaseForm.vue'),
+                //     meta: { title: '帖子管理' }
+                // },
+                // {
+                //     // 富文本编辑器组件
+                //     path: '/editor',
+                //     component: () => import(/* webpackChunkName: "editor" */ '../components/page/VueEditor.vue'),
+                //     meta: { title: '富文本编辑器' }
+                // },
+                // {
+                //     // markdown组件
+                //     path: '/markdown',
+                //     component: () => import(/* webpackChunkName: "markdown" */ '../components/page/Markdown.vue'),
+                //     meta: { title: 'markdown编辑器' }
+                // },
+                // {
+                //     // 图片上传组件
+                //     path: '/upload',
+                //     component: () => import(/* webpackChunkName: "upload" */ '../components/page/Upload.vue'),
+                //     meta: { title: '文件上传' }
+                // },
+                {
+                    // vue-schart组件
+                    path: '/charts',
+                    component: () => import(/* webpackChunkName: "chart" */ '../components/page/BaseCharts.vue'),
+                    meta: { title: 'schart图表' }
+                },
+                {
+                    // 拖拽列表组件
+                    path: '/drag',
+                    component: () => import(/* webpackChunkName: "drag" */ '../components/page/DragList.vue'),
+                    meta: { title: '拖拽列表' }
+                },
+                {
+                    // 拖拽Dialog组件
+                    path: '/dialog',
+                    component: () => import(/* webpackChunkName: "dragdialog" */ '../components/page/DragDialog.vue'),
+                    meta: { title: '拖拽弹框' }
+                },
+                {
+                    // 国际化组件
+                    path: '/i18n',
+                    component: () => import(/* webpackChunkName: "i18n" */ '../components/page/I18n.vue'),
+                    meta: { title: '国际化' }
+                },
+                {
+                    // 权限页面
+                    path: '/permission',
+                    component: () => import(/* webpackChunkName: "permission" */ '../components/page/Permission.vue'),
+                    meta: { title: '权限测试', permission: true }
+                },
+                {
+                    path: '/404',
+                    component: () => import(/* webpackChunkName: "404" */ '../components/page/404.vue'),
+                    meta: { title: '404' }
+                },
+                {
+                    path: '/403',
+                    component: () => import(/* webpackChunkName: "403" */ '../components/page/403.vue'),
+                    meta: { title: '403' }
+                },
+
+            ]
+        },
+        {
+            path: '/login',
+            component: () => import(/* webpackChunkName: "login" */ '../components/page/Login.vue'),
+            meta: { title: '登录' }
+        },
+        {
+            path: '*',
+            redirect: '/404'
+        }
+    ]
+});
